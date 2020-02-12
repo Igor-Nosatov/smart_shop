2e1be3a36dedef92997f8d5ef849a518439cb4a1
Igor-Nosatov <sinevor13@gmail.com>
2020-02-12 19:40:44 +0200

    seventh part

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class BasketController extends Controller
 {
-    public function basket() 
+    public function basket()
     {
         $orderId = session('orderId');
         if (!is_null($orderId)) {
@@ -16,22 +16,50 @@ class BasketController extends Controller
         return view('basket', compact('order'));
     }
 
-    public function basketPlace() {
+    public function basketPlace()
+    {
         return view('order');
     }
 
     public function basketAdd($productId)
     {
-       $orderId = session('orderId');
-       if(is_null($orderId)){
-           $order = Order::create()->id;
-           session(['orderId' => $order->id]);
-       }else{
-        $order = Order::find($orderId);
-       }
-       $order->products()->attach($productId);
-       
-       return view('basket', compact('order'));
+        $orderId = session('orderId');
+        if (is_null($orderId)) {
+            $order = Order::create();
+            session(['orderId' => $order->id]);
+        } else {
+            $order = Order::find($orderId);
+        }
 
+        if ($order->products->contains($productId)) {
+            $pivotRow = $order->products()->where('product_id', $productId)->first()->pivot;
+            $pivotRow->count++;
+            $pivotRow->update();
+        } else {
+            $order->products()->attach($productId);
+        }
+
+        return redirect()->route('basket');
+    }
+
+    public function basketRemove($productId)
+    {
+        $orderId = session('orderId');
+        if (is_null($orderId)) {
+            return redirect()->route('basket');
+        }
+        $order = Order::find($orderId);
+
+        if ($order->products->contains($productId)) {
+            $pivotRow = $order->products()->where('product_id', $productId)->first()->pivot;
+            if ($pivotRow->count < 2) {
+                $order->products()->detach($productId);
+            } else {
+                $pivotRow->count--;
+                $pivotRow->update();
+            }
+        }
+
+        return redirect()->route('basket');
     }
 }
